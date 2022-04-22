11b7f4279b9ab297def097101f6fc5911aa9dc30
Baldwin Ngo <baldwinngo0813@gmail.com>
2022-04-21 20:41:17 -0700

    Initialize project and start forecast api call functions

---
+++ forecast.js
@@ -0,0 +1,27 @@
+const key = 'bHPoIYD5TL5UbIF0x4O0Bvi46nq6bFM3';
+const city = 'miami';
+
+
+const getCity = async (city) => {
+  
+  const resourceBase = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const resourceQuery = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(resourceBase + resourceQuery);
+  const data = await response.json();
+
+  return (data[0].Key)
+}
+
+
+const getWeather = async (locationKey) => {
+
+  const resourceBase = 'http://dataservice.accuweather.com/currentconditions/v1/'
+  const resourceQuery = `${locationKey}?apikey=${key}`
+
+  const response = await fetch(resourceBase + resourceQuery);
+  const data = await response.json();
+
+}
+
+console.log(getCity(city).then(data => data))
